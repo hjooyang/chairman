5fca90ef0fed3165d08a37b93d483519bf32bfef
hisgracee <hyjyang@kr.ibm.com>
2015-09-09 19:52:21 +0900

    initial commit login page

--- README.md
+++ README.md
@@ -1,3 +1,71 @@
+<<<<<<< HEAD
+# ionic Memory Game
+
+Memory game built in Ionic, Angular and Cordova
+![ionic-memory-game screenshots](https://raw.github.com/segra/ionic-memory-game/gh-pages/images/screenshots.png)
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Features](#features)
+3. [Demo](#demo)
+4. [TODO](#todo)
+5. [License](#license)
+
+
+### Installation
+To use this project, first clone the repo, then run in local repo:
+```bash
+$ sudo npm install -g cordova ionic gulp
+$ npm install
+$ gulp install
+```
+
+View in browser
+
+```
+ionic serve
+```
+
+**[Back to top](#table-of-contents)**
+
+
+## Features (wip)
+* local db (JSON)
+* local storage
+* select your game's color theme
+* ionic modals and popups
+* event broadcasting
+
+**[Back to top](#table-of-contents)**
+
+## Demo
+an online demo of this app is available at http://segra.github.io/ionic-memory-game/
+
+## TODO
+* timed game mode
+* game statistics
+* add sound effects
+* refactor ‘theme’ implementation to reduce watchers
+* improve local storage offline syncing
+* create a splash screen and icon
+
+**[Back to top](#table-of-contents)**
+
+## License
+
+Licensed under the MIT Open Source license. For more information, see the LICENSE file in this repository.
+
+**[Back to top](#table-of-contents)**
+
+
+
+
+
+
+
+
+=======
 This README.md file is displayed on your project page. You should edit this 
 file to describe your project, including instructions for building and 
 running the project, pointers to the license under which you are making the 
@@ -15,3 +83,4 @@ eye-catching graphic.
 
 Finally, consider defining some sprints and work items in Track & Plan to give 
 interested developers a sense of your cadence and upcoming enhancements.
+>>>>>>> 77f4d2f0946cbfdc7799c985c8912b324a1436bc
---
+++ www/js/controllers.js
@@ -0,0 +1,269 @@
+(function() {
+	'use strict';
+
+	angular.module('memory.controllers', [])
+
+	.controller('AppCtrl', ['$scope', '$ionicModal', '$ionicPopup', '$state', '$cookieStore', '$window', 'Settings', function($scope, $ionicModal, $ionicPopup, $state, $cookieStore, $window, Settings) {
+		Settings.set('gameOn','false');
+		$scope.settings = Settings.getSettings();
+		/*** Settings modal ***/
+		$scope.showSettings = function() {
+			if(!$scope.settingsModal) {
+				// Load the modal from the given template URL
+				$ionicModal.fromTemplateUrl('templates/settings.html', function(modal) {
+					$scope.settingsModal = modal;
+					$scope.settingsModal.show();
+				}, {
+					// The animation we want to use for the modal entrance
+					animation: 'slide-in-up'
+				});
+			} else {
+				$scope.settingsModal.show();
+			}
+		};
+		/*** Info modal ***/
+		$scope.showInfo = function() {
+			if(!$scope.infoModal) {
+				// Load the modal from the given template URL
+				$ionicModal.fromTemplateUrl('templates/info.html', function(modal) {
+					$scope.infoModal = modal;
+					$scope.infoModal.show();
+				}, {
+					// The animation we want to use for the modal entrance
+					animation: 'slide-in-up'
+				});
+			} else {
+				$scope.infoModal.show();
+			}
+		};
+		/*** Check if a game is in progress ***/
+		$scope.checkGame = function() {
+			var stateName = $state.current.name.substring(0, 4);
+			var gameState = Settings.get('gameOn');
+			console.log('checkGame: ',stateName, gameState);
+			if (stateName === 'game' && gameState) {
+				$scope.showConfirm();
+			} else {
+				$state.go('home');
+			}
+		};
+		/*** Game Quit Confirmation popup ***/
+		$scope.showConfirm = function() {
+			console.log('showConfirm');
+			var confirmPopup = $ionicPopup.confirm({
+				title: 'Are you sure you want to quit?',
+				cancelText: 'Yes',
+				cancelType: 'button-outline',
+				okText: 'No',
+				okType: 'button-positive',
+	  			scope: $scope,
+
+			});
+			confirmPopup.then(function(res) {
+				if(res) {
+					$state.go($state.current, {}, { reload: false });
+				} else {
+					Settings.set('gameOn','false');
+					$state.go('home');
+				}
+			});
+		};
+
+		// Logout user
+	    $scope.logout = function () {
+	        $cookieStore.remove("userInfo");
+	        $state.go('welcome');
+	        $window.location.reload();
+	    };
+	}])
+
+	.controller('HomeCtrl', ['$scope', 'Settings', function($scope, Settings) {
+		$scope.settings = Settings.getSettings();
+		$scope.theme = Settings.get('theme');
+	}])
+
+	.controller('GameCtrl', ['$scope', '$state', '$ionicPopup', 'icons', 'Settings', function($scope, $state, $ionicPopup, icons, Settings) {
+		//	Retrieve tile icons and save to $scope & local settings
+		$scope.icons = icons;
+		Settings.set('icons', icons);
+
+	  	$scope.settings = Settings.getSettings();
+
+		// Listeners for game events triggered by angular-memory-game
+		$scope.$on('memoryGameUnmatchedPairEvent', function() {
+			$scope.message = 'Try again!';
+			console.log($scope.message);
+		});
+		$scope.$on('memoryGameMatchedPairEvent', function() {
+			$scope.message = 'Good match!';
+			console.log($scope.message);
+		});
+		$scope.$on('memoryGameCompletedEvent', function() {
+			$scope.message = 'Success!';
+			$scope.showFinale();
+			console.log($scope.message);
+		});
+		$scope.$on('memoryGameIconErrorEvent', function() {
+			$scope.message = 'ERROR: # of available tiles is less than the grid requires';
+			$scope.showIconError();
+			console.log($scope.message);
+		});
+
+		/*** Game Over popup ***/
+		$scope.showFinale = function() {
+			var finalePopup = $ionicPopup.confirm({
+				title: '<h2>Congratulations!</h2><h4>You matched all the tiles!</h4>',
+				cancelText: 'Main Menu',
+				cancelType: 'button-dark',
+				okText: 'Play Again',
+				okType: 'button-balanced',
+	  			scope: $scope,
+			});
+			finalePopup.then(function(res) {
+				if(res) {
+					console.log('Play Again');
+					$state.go($state.current, {}, { reload: true });
+				} else {
+					console.log('Main Menu');
+					$state.go('home');
+				}
+			});
+		};
+
+		/*** Error popup ***/
+		$scope.showIconError = function() {
+			var errorPopup = $ionicPopup.confirm({
+				title: '<h2>ERROR:</h2><h4>there was a problem loading the icons.</h4>',
+				cancelText: 'Main Menu',
+				cancelType: 'button-dark',
+				okText: 'Try Again?',
+				okType: 'button-balanced',
+	  			scope: $scope,
+			});
+			errorPopup.then(function(res) {
+				if(res) {
+					console.log('Try Again?');
+					$state.go($state.current, {}, { reload: true });
+				} else {
+					console.log('Main Menu');
+					$state.go('home');
+				}
+			});
+		};
+
+	}])
+
+	.controller('FinaleCtrl', ['$scope', '$state', 'Settings', function($scope, $state, Settings) {
+		$scope.settings = Settings.getSettings();
+		$scope.restartBtn = function() {
+			console.log('restartBtn');
+			$state.go($state.current, {}, { reload: false });
+			$scope.modal.hide();
+		};
+
+		$scope.closeInfo = function() {
+			$scope.modal.hide();
+		};
+	}])
+
+	.controller('InfoCtrl', ['$scope', 'Settings', function($scope, Settings) {
+		$scope.settings = Settings.getSettings();
+		$scope.closeInfo = function() {
+			$scope.modal.hide();
+		};
+	}])
+
+	.controller('SettingsCtrl', ['$scope', 'Settings', function($scope, Settings) {
+		$scope.settings = Settings.getSettings();
+	    $scope.themes = Settings.getAllThemes();
+
+		// Watch deeply for settings changes, and save them if necessary
+		$scope.$watch('settings', function(v) {
+			Settings.save();
+			console.log('settings.change', Settings.get('theme'));
+		}, true);
+
+		$scope.closeSettings = function() {
+			$scope.modal.hide();
+		};
+	}])
+	.controller('WelcomeCtrl', function ($scope, $state, $cookieStore) {
+		
+	    /**
+	     * SOCIAL LOGIN
+	     * Facebook and Google
+	     */
+	    // FB Login
+	    $scope.fbLogin = function () {
+                alert("before login");
+	        FB.login(function (response) {
+                     alert("login");
+	            if (response.authResponse) {
+	                getUserInfo();
+	            } else {
+	                console.log('User cancelled login or did not fully authorize.');
+	            }
+	        }, {scope: 'email,user_photos,user_videos'});
+
+	        function getUserInfo() {
+	            // get basic info
+	            FB.api('/me', function (response) {
+	                console.log('Facebook Login RESPONSE: ' + angular.toJson(response));
+	                // get profile picture
+	                FB.api('/me/picture?type=normal', function (picResponse) {
+	                    console.log('Facebook Login RESPONSE: ' + picResponse.data.url);
+	                    response.imageUrl = picResponse.data.url;
+	                    // store data to DB - Call to API
+	                    // Todo
+	                    // After posting user data to server successfully store user data locally
+	                    var user = {};
+	                    user.name = response.name;
+	                    user.email = response.email;
+	                    if(response.gender) {
+	                        response.gender.toString().toLowerCase() === 'male' ? user.gender = 'M' : user.gender = 'F';
+	                    } else {
+	                        user.gender = '';
+	                    }
+	                    user.profilePic = picResponse.data.url;
+	                    $cookieStore.put('userInfo', user);
+	                    // $state.go('dashboard');
+	                    $state.go('home');
+
+	                });
+	            });
+	        }
+	    };
+	    // END FB Login
+	})
+	.controller('LoginCtrl', ['$scope', 'LoginService','$ionicPopup', '$state', function ($scope, LoginService,$ionicPopup, $state) {
+		$scope.data = {};
+ 
+	    $scope.login = function() {
+	        LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+	            $state.go('tab.dash');
+	        }).error(function(data) {
+	            var alertPopup = $ionicPopup.alert({
+	                title: 'Login failed!',
+	                template: 'Please check your credentials!'
+	            });
+	        });
+	    }
+	}])
+	// .controller('LoginCtrl', function($scope, LoginService) {
+	//     $scope.data = {};
+	 
+	//     $scope.login = function() {
+
+	//     	 LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+	//             $state.go('tab.dash');
+	//         }).error(function(data) {
+	//             var alertPopup = $ionicPopup.alert({
+	//                 title: 'Login failed!',
+	//                 template: 'Please check your credentials!'
+	//             });
+	//         });
+
+	//         console.log("LOGIN user: " + $scope.data.username + " - PW: " + $scope.data.password);
+	//     }
+	// });
+}());
